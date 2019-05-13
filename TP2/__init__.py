ce5f460f09683bac668ebe111cf583678670b05e
Marcelo Castellano <marce.geek22@gmail.com>
2019-05-13 14:29:41 -0300

    simplificar código del TP2 e implementar generación de gráficos LaTeX con matplotlib2tikz

--- TP2/__init__.py
+++ TP2/__init__.py
@@ -1,61 +1,122 @@
+import abc
+
 import numpy as np
-
 import matplotlib.pyplot as plt
+import matplotlib2tikz
 
-UNIFORM, EXPONENTIAL, NORMAL = range(1, 3 + 1)
 
+class RandomPopulation(abc.ABC):
 
-class RandomPopulation:
-    SEED = 3619001274
-
-    def __init__(self, n):
+    def __init__(self, n=1000, seed=None):
         # fijar semilla aleatoria para asegurar la repetibilidad
-        np.random.seed(self.SEED)
-        self.uniform_base = np.random.sample(n)
-        self._bins = None
-
-    def map_methods(self, method):
-        if method == UNIFORM:
-            return self.uniform_random, self.uniform_pdf, lambda pop, a, b: (a, b)
-        elif method == EXPONENTIAL:
-            return self.exponential_random, self.exponential_pdf, self.exponential_xrange
+        self.generator = np.random.RandomState(seed)
+        self._population = self.generator.random_sample(n)
+        self._generated = False
+        self._classes = None
 
     @property
-    def bins(self):
-        if self._bins is None:
-            n = len(self.uniform_base)
+    def population(self):
+        if not self._generated:
+            self._population = self._gen_distribution(self._population)
+            self._generated = True
+        return self._population
+
+    @abc.abstractmethod
+    def _gen_distribution(self, population):
+        return population
+
+    @abc.abstractmethod
+    def theoric_distribution(self, x):
+        return [1.] * len(x)
+
+    @abc.abstractmethod
+    def x_range(self):
+        # rango empírico
+        return min(self.population), max(self.population)
+
+    @property
+    def classes(self):
+        if self._classes is None:
+            n = len(self.population)
             sq = np.sqrt(n)
-            self._bins = int(np.ceil(sq))
-        return self._bins
+            self._classes = int(np.ceil(sq))
+        return self._classes
 
-    @staticmethod
-    def uniform_pdf(x, a, b):
-        return [1. / (b - a)] * len(x)
-
-    def uniform_random(self, a, b):
-        return a + (b - a) * self.uniform_base
-
-    @staticmethod
-    def exponential_pdf(x, alpha):
-        return alpha * np.exp(-alpha * x)
-
-    @staticmethod
-    def exponential_xrange(pop, alpha):
-        return 0, max(5. / alpha, max(pop))
-
-    def exponential_random(self, alpha):
-        return -1. / alpha * np.log(self.uniform_base)
-
-    def normal_random(self):
-        pass
-
-    def plot(self, method, *args):
-        rnd, pdf, rng = self.map_methods(method)
-        res = rnd(*args)
-        plt.hist(res, self.bins, density=True)
-        min_x, max_x = rng(res, *args)
-        x = np.linspace(min_x, max_x, np.round((max_x - min_x) * 10))
-        theorical = pdf(x, *args)
-        plt.plot(x, theorical)
+    def histogram(self, latexfile=None, standalone_latex=False):
+        plt.cla()
+        plt.hist(self.population, bins=self.classes, density=True)
+        x = np.linspace(*self.x_range(), 1000)
+        plt.plot(x, self.theoric_distribution(x))
         plt.grid()
-        plt.show()
+        if latexfile is None:
+            plt.show()
+        else:
+            matplotlib2tikz.save(latexfile,
+                                 extra_axis_parameters=[
+                                     'scaled ticks=false',
+                                     'xticklabel style={/pgf/number format/.cd,fixed,precision=2}',
+                                     'yticklabel style={/pgf/number format/.cd,fixed,precision=2}'
+                                 ],
+                                 standalone=standalone_latex)
+
+
+class UniformRandomPopulation(RandomPopulation):
+
+    def __init__(self, a, b, n=1000, seed=None):
+        super().__init__(n=n, seed=seed)
+        self.a = a
+        self.b = b
+
+    def _gen_distribution(self, population):
+        return self.a + (self.b - self.a) * population
+
+    def theoric_distribution(self, x):
+        return [1. / (self.b - self.a)] * len(x)
+
+    def x_range(self):
+        return self.a, self.b
+
+
+class ExponentialRandomPopulation(RandomPopulation):
+
+    def __init__(self, alpha, n=1000, seed=None):
+        super().__init__(n=n, seed=seed)
+        self.alpha = alpha
+
+    def _gen_distribution(self, population):
+        return -1. / self.alpha * np.log(population)
+
+    def theoric_distribution(self, x):
+        return self.alpha * np.exp(-self.alpha * x)
+
+    def x_range(self):
+        return 0, max(5. / self.alpha, max(self.population))
+
+
+class NormalRandomPopulation(RandomPopulation):
+
+    def __init__(self, mu, sigma, n=1000, seed=None):
+        super().__init__(n=n, seed=seed)
+        self.mu = mu
+        self.sigma = sigma
+
+    def _gen_distribution(self, population):
+        couples = np.split(population, len(population) // 2)
+        dist = []
+        for u1, u2 in couples:
+            assert isinstance(u1, float)
+            assert isinstance(u2, float)
+            r = np.sqrt(-2 * np.log(u1))
+            z0, z1 = r * np.cos(2 * np.pi * u2), r * np.sin(2 * np.pi * u2)
+            x0 = z0 * self.sigma + self.mu
+            x1 = z1 * self.sigma + self.mu
+            dist.append(x0)
+            dist.append(x1)
+        return dist
+
+    def theoric_distribution(self, x):
+        return 1 / np.sqrt(2 * np.pi * self.sigma ** 2, ) * np.exp(-(x - self.mu) ** 2 / (2 * self.sigma ** 2))
+
+    def x_range(self):
+        delta = 5 * self.sigma
+        return self.mu - delta, self.mu + delta
--- TP2/program.py
+++ TP2/program.py
@@ -2,9 +2,12 @@ import TP2
 
 
 def main():
-    rnd = TP2.RandomPopulation(1000)
-    rnd.plot(TP2.UNIFORM, 10, 35)
-    rnd.plot(TP2.EXPONENTIAL, 2)
+    rnd = TP2.UniformRandomPopulation(10, 35, n=1000, seed=228547833)
+    rnd.histogram(latexfile='tex/uniform.tex')
+    rnd = TP2.ExponentialRandomPopulation(3, n=1000, seed=1957336057)
+    rnd.histogram(latexfile='tex/exponential.tex')
+    rnd = TP2.NormalRandomPopulation(3, 2, n=1000, seed=1085907535)
+    rnd.histogram(latexfile='tex/normal.tex')
 
 
 if __name__ == '__main__':
