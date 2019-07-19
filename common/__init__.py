dbf32984874edf087aceeefe49db9fd7e6b91269
Marcelo Castellano <marce.geek22@gmail.com>
2019-07-18 21:07:54 -0300

    mejorar manejo y mostrado de poblaciones aleatorias

--- common/__init__.py
+++ common/__init__.py
@@ -8,7 +8,7 @@ class RandomPopulation:
         self._population = np.array(population)
 
     @property
-    def population(self):
+    def values(self):
         return self._population.copy()
 
     @property
@@ -17,6 +17,18 @@ class RandomPopulation:
         sq = np.sqrt(n)
         return int(np.ceil(sq))
 
+    def is_continuous(self):
+        return not self.is_discrete()
+
+    def is_discrete(self):
+        return self._population.dtype.kind == 'i'
+
     def plot_histogram(self):
-        plt.hist(self._population, bins=self.classes, density=True)
+        if self.is_continuous():
+            plt.hist(self._population, bins=self.classes, density=True)
+        else:
+            dist = self.frequency_distribution()
+            # tomar y avanzar el color
+            ax, = plt.plot(dist.values, dist.frequencies, ',')
+            plt.vlines(dist.values, 0, dist.frequencies, lw=10, colors=ax.get_color())
         plt.grid()
