bd849b731c3b87be8230e555283c6308b7e8f909
Marcelo Castellano <marce.geek22@gmail.com>
2019-08-23 00:34:15 -0300

    implementar distribución gamma y métodos adicionales de las distribuciones

--- TP2/__init__.py
+++ TP2/__init__.py
@@ -1,6 +1,7 @@
 import abc
 
 import numpy as np
+import scipy.special
 
 from common import RandomPopulation
 
@@ -20,11 +21,11 @@ class ProbabilityDistribution(abc.ABC):
 
     def plot_theoretical(self, axes, label=None):
         if self.is_continuous():
-            x = np.linspace(*self._value_range(), 1000)
-            axes.plot(x, self._theoretical_distribution(x), label=label)
+            x = np.linspace(*self._value_range(), num=1000)
+            axes.plot(x, self.theoretical_distribution(x), label=label)
         else:
             x = np.arange(self._value_range()[0], self._value_range()[1] + 1)
-            axes.plot(x, self._theoretical_distribution(x), 'o', label=label)
+            axes.plot(x, self.theoretical_distribution(x), 'o', label=label)
 
     def random_sample(self, size=None):
         if size is not None:
@@ -32,12 +33,59 @@ class ProbabilityDistribution(abc.ABC):
         return next(self.generator)
 
     @abc.abstractmethod
+    def theoretical_distribution(self, x):
+        return np.array([1.] * len(x))
+
+    def cumulative_distribution(self, x):
+        if np.ndim(x) == 0:
+            return self._cdf(x)
+        else:
+            r = np.zeros(len(x))
+            for i in range(len(x)):
+                r[i] = self.cumulative_distribution(x[i])
+            return r
+
+    def percent_point(self, p):
+        if np.ndim(p) == 0:
+            return self._ppf(p)
+        else:
+            r = np.zeros(len(p))
+            for i in range(len(p)):
+                r[i] = self.percent_point(p[i])
+            return r
+
+    @abc.abstractmethod
+    def mean(self):
+        return .5
+
+    @abc.abstractmethod
+    def variance(self):
+        return 1/12.
+
+    def standard_deviation(self):
+        return np.sqrt(self.variance())
+
+    @abc.abstractmethod
     def _value_range(self):
         return 0., 1.
 
     @abc.abstractmethod
-    def _theoretical_distribution(self, x):
-        return np.array([1.] * len(x))
+    def _cdf(self, x):
+        # implementación básica por defecto
+        x0, _ = self._value_range()
+        v = np.linspace(x0, x, num=1000)
+        y = self.theoretical_distribution(v)
+        return np.trapz(y, v)
+
+    @abc.abstractmethod
+    def _ppf(self, p):
+        # implementación básica por defecto
+        first, last = self._value_range()
+        x = np.linspace(first, last, num=1000)
+        for i in range(len(x)):
+            if self._cdf(x) >= p:
+                return x[i]
+        return np.nan
 
     def _generator(self):
         while True:
@@ -56,11 +104,30 @@ class UniformDistribution(ProbabilityDistribution):
         self.a = a
         self.b = b
 
+    def theoretical_distribution(self, x):
+        # FIXME corregir probabilidad para valores fuera de rango
+        return np.array([1. / (self.b - self.a)] * len(x))
+
+    def mean(self):
+        return (self.a + self.b)/2.
+
+    def variance(self):
+        return (self.b - self.a)**2/12.
+
     def _value_range(self):
         return self.a, self.b
 
-    def _theoretical_distribution(self, x):
-        return [1. / (self.b - self.a)] * len(x)
+    def _cdf(self, x):
+        if x < self.a:
+            return 0.
+        elif x > self.b:
+            return 1.
+        return (x - self.a)/(self.b - self.a)
+
+    def _ppf(self, p):
+        if not 0 <= p <= 1:
+            return np.nan
+        return self.a + (self.b - self.a) * p
 
     def _distribution_values(self):
         u = self.state.random_sample()
@@ -74,16 +141,66 @@ class ExponentialDistribution(ProbabilityDistribution):
         super().__init__(seed=seed)
         self.alpha = alpha
 
+    def theoretical_distribution(self, x):
+        return self.alpha * np.exp(-self.alpha * x)
+
+    def mean(self):
+        return 1./self.alpha
+
+    def variance(self):
+        return self.mean()**2
+
     def _value_range(self):
         # cubrir el 99.9% de los valores
         return 0, 7. / self.alpha
 
-    def _theoretical_distribution(self, x):
-        return self.alpha * np.exp(-self.alpha * x)
+    def _cdf(self, x):
+        return super()._cdf(x)
+
+    def _ppf(self, p):
+        return super()._ppf(p)
 
     def _distribution_values(self):
         u = self.state.random_sample()
         r = -1. / self.alpha * np.log(u)
+        return [r]
+
+
+class GammaDistribution(ProbabilityDistribution):
+
+    def __init__(self, k, alpha, seed=None):
+        super().__init__(seed=seed)
+        self.alpha = alpha
+        self.k = k
+
+    def theoretical_distribution(self, x):
+        return self.alpha ** self.k * x ** (self.k - 1) * np.exp(-self.alpha * x) / scipy.special.gamma(self.k)
+
+    def mean(self):
+        return self.k/self.alpha
+
+    def variance(self):
+        return self.k/self.alpha**2
+
+    def _value_range(self):
+        first = self.percent_point(0.0001)
+        last = self.percent_point(0.9999)
+        return first, last
+
+    def _cdf(self, x):
+        return scipy.special.gammaincc(self.k, 0) - scipy.special.gammaincc(self.k, self.alpha * x)
+
+    def _ppf(self, p):
+        return scipy.special.gammainccinv(self.k, scipy.special.gammaincc(self.k, 0) - p)/self.alpha
+
+    def _distribution_values(self):
+        k = int(np.floor(self.k))
+        if k != self.k:
+            q = self.k - k
+            if self.state.random_sample() < q:
+                k += 1
+        u = self.state.random_sample(k)
+        r = -1. / self.alpha * np.log(u.prod())
         return [r]
 
 
@@ -94,13 +211,25 @@ class NormalDistribution(ProbabilityDistribution):
         self.mu = mu
         self.sigma = sigma
 
+    def theoretical_distribution(self, x):
+        return 1 / np.sqrt(2 * np.pi * self.sigma ** 2) * np.exp(-(x - self.mu) ** 2 / (2 * self.sigma ** 2))
+
+    def mean(self):
+        return self.mu
+
+    def variance(self):
+        return self.sigma**2
+
     def _value_range(self):
         # cubrir el 99.99% de los valores
         delta = 4 * self.sigma
         return self.mu - delta, self.mu + delta
 
-    def _theoretical_distribution(self, x):
-        return 1 / np.sqrt(2 * np.pi * self.sigma ** 2) * np.exp(-(x - self.mu) ** 2 / (2 * self.sigma ** 2))
+    def _cdf(self, x):
+        return super()._cdf(x)
+
+    def _ppf(self, p):
+        return super()._ppf(p)
 
     def _distribution_values(self):
         u1 = self.state.random_sample()
@@ -124,6 +253,20 @@ class EmpiricalDistribution(ProbabilityDistribution):
         # primera clase/valor
         self.cumulative = np.insert(cumulative, 0, -1.)
 
+    def theoretical_distribution(self, x):
+        arr = np.zeros(len(x))
+        if not self.is_continuous():
+            freqs = self.distribution.frequencies
+        for i in range(len(x)):
+            arr[i] = self.distribution.get_freq(x[i])
+        return arr
+
+    def mean(self):
+        pass  # TODO implementar
+
+    def variance(self):
+        pass  # TODO implementar
+
     def _value_range(self):
         first = self.distribution[0]
         last = self.distribution[-1]
@@ -131,13 +274,11 @@ class EmpiricalDistribution(ProbabilityDistribution):
             return first.val[0], last.val[1]
         return first.val, last.val
 
-    def _theoretical_distribution(self, x):
-        arr = np.zeros(len(x))
-        if not self.is_continuous():
-            freqs = self.distribution.frequencies
-        for i in range(len(x)):
-            arr[i] = self.distribution.get_freq(x[i])
-        return arr
+    def _cdf(self, x):
+        return super()._cdf(x)
+
+    def _ppf(self, p):
+        return super()._ppf(p)
 
     def _distribution_values(self):
         r = self.state.random_sample()
@@ -174,6 +315,13 @@ class ExponentialRandomPopulation(RandomDistributionPopulation):
 
     def __init__(self, alpha, n=1000, seed=None):
         super().__init__(ExponentialDistribution(alpha, seed=seed),
+                         n=n)
+
+
+class GammaRandomPopulation(RandomDistributionPopulation):
+
+    def __init__(self, k, alpha, n=1000, seed=None):
+        super().__init__(GammaDistribution(k, alpha, seed=seed),
                          n=n)
 
 
